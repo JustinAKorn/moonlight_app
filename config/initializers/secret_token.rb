0b89b0b15c656424f523d91fa2394c0ecd2666b9
JustinAKorn <justinakorn@gmail.com>
2013-06-06 13:02:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoonlightApp::Application.config.secret_token = '28c69481e74dbd8168b7a6ffba6febd8b08d9bbfacecf32382dc345ab607d46ebf91bc9b5ad397ca4925b2939afaa6634d7442ba82251ef0fb01c5b39e24538a'
